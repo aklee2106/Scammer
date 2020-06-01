ee82110e6056fc26fe6b88f1a2614f74c0337400
Alex Lee <aklee2106@gmail.com>
2020-06-01 18:34:03 -0400

    changed entry file h1

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Yabber</h1>, root);
+  ReactDOM.render(<h1>Cody smells</h1>, root);
 });
