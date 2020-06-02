0f0c3d2c3adf6af0c6da12589196e0bf8ddb7c80
Alex Lee <aklee2106@gmail.com>
2020-06-01 23:09:17 -0400

    Log in and Sign up links/routes rendering"

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import GreetingContainer from './greeting/greeting_container';
+import {Route} from 'react-router-dom'; 
+import SignUpFormContainer from './session_form/signup_form_container';
 
 const App = () => (
   <div>
-    <h1>This is coming from App.jsx</h1>
+    <header>
+      <h1>This is coming from App.jsx</h1>
+      <GreetingContainer /> 
+    </header>
+
+    {/* <Route path="/login" component={SignupFormContainer} /> */}
+
   </div>
 );
 
