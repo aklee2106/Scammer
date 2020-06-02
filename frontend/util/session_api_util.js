603342c1b4a9afec720e850e0d6ea1f47410eb02
Alex Lee <aklee2106@gmail.com>
2020-06-01 21:08:56 -0400

    got app.jsx rendering

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,23 +1,49 @@
-$.ajax({
-  method: 'POST', 
-  url: '/api/users',
-  data: {user: {
-    email: 'alex@work.com',
-    password: 'hunter12'
-  }}
-}) 
+export const signup = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/user',
+    data: { user }
+  })
+);
 
-$.ajax({
-  method: 'POST', 
-  url: '/api/session',
-  data: {user: {
-    email: 'alex@work.com',
-    password: 'hunter12'
-  }}
-}) 
+export const login = user => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/session',
+    data: { user }
+  })
+);
 
-$.ajax({
-  method: "DELETE",
-  url: "/api/session",
-}) 
+
+export const logout = () => (
+  $.ajax({
+    method: 'DELETE',
+    url: '/api/session'
+  })
+);
+
+
+
+// $.ajax({
+//   method: 'POST', 
+//   url: '/api/users',
+//   data: {user: {
+//     email: 'alex@work.com',
+//     password: 'hunter12'
+//   }}
+// }) 
+
+// $.ajax({
+//   method: 'POST', 
+//   url: '/api/session',
+//   data: {user: {
+//     email: 'alex@work.com',
+//     password: 'hunter12'
+//   }}
+// }) 
+
+// $.ajax({
+//   method: "DELETE",
+//   url: "/api/session",
+// }) 
 
