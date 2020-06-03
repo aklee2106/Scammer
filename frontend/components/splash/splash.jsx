8e0bc9749be97a793c06c6f6ec4bddbb2ac58af6
Alex Lee <aklee2106@gmail.com>
2020-06-02 21:27:56 -0400

    css touches to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Splash extends React.Component {
   render (){
     return (
       <div className="splash-container">
-        <header>
+        <header className="splash-header">
           <nav className="splash-nav">
-            
-            <ul>
+            <img src= {window.logo2}/>
 
+            <ul>
+              <li><a href="https://www.microsoft.com/en-us/microsoft-365/business/all-business">Microsoft 365</a></li>
+              <li><a href="https://azure.microsoft.com/en-us/">Azure</a></li>
+              <li><a href="https://www.microsoft.com/en-us/microsoft-365/business/all-business">Office 365</a></li>
+              <li><a href="https://dynamics.microsoft.com/en-us/">Dynamics 365</a></li>
+              <li><a href="https://www.microsoft.com/en-us/sql-server/">SQL</a></li>
+              <li><a href="https://www.microsoft.com/en-us/microsoft-365/windows">Windows 10</a></li>
             </ul>
           </nav>
         </header>
 
         <main className="splash-content">
           <nav className="splash-content-nav">
+            <h2>Yabber</h2>
+            <div className="splash-button-div">
+              <button className="splash-button">Buy Office 365</button>
+              <button className="splash-button" id="log-in"><i class="fas fa-user"></i> Log In</button>
 
+              
+              <Link className="splash-button" id="log-in"
+                 to="/login"><i class="fas fa-user"></i> Log In</Link>
+  
+            </div>
           </nav>
 
-          <div>
+          <div className="splash-content-div">
             <h1>Work Smarter, Work Together</h1>
             <p>Connect with people across your organization 
               to make better decisions, faster.
