5f2c1109d3fd78625c1dfb2939a5848893d9a215
Alex Lee <aklee2106@gmail.com>
2020-07-01 17:49:47 -0400

    remove debugger from convo index

--- frontend/components/feed/conversation_index.jsx
+++ frontend/components/feed/conversation_index.jsx
@@ -5,18 +5,14 @@ import CommentFormContainer from '../comments/comment_form_container';
 
 class ConversationIndex extends React.Component {
   componentDidMount() {
-    
-    //so this will update global state. then because state updates, mSTP fires up again
-    //and changes this component's local state? 
     this.props.fetchConversations();
     this.props.fetchUsers();
   }
 
   render() {
-    // debugger
+  
     const { conversations, users, currentGroup, currentUser} = this.props;
 
-    //what happens if this were not here? First time it renders, error out? 
     if (Object.values(users).length <2 ){
       return null;
     }
